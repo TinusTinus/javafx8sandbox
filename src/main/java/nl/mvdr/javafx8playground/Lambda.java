75ce262302e1440248686009ad24d599ca5d8e44
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-02-21 17:27:01 +0100

    Added a simple program for experimenting with lambdas.

---
+++ src/main/java/nl/mvdr/javafx8playground/Lambda.java
@@ -0,0 +1,43 @@
+package nl.mvdr.javafx8playground;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * Playing around with lambdas in JavaFX 8.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class Lambda extends Application {
+    /** {@inheritDoc} */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button button = new Button("Press me!");
+
+        // Without lambdas we would have to write something like:
+//        button.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Button pressed.");
+//            }
+//        });
+        
+        // With lambdas we can simply write:
+        button.setOnAction(event -> System.out.println("Button pressed."));
+        
+        primaryStage.setTitle("Lambda");
+        primaryStage.setScene(new Scene(button));
+        primaryStage.show();
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline parameters; these are passed on to JavaFX
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
