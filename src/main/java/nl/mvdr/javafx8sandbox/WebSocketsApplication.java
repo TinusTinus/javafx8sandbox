fc1175b9b782ed3e46d69a2c641fbb98f927533b
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-02-27 13:09:13 +0100

    WebSocket-voorbeeld toegevoegd.

---
+++ src/main/java/nl/mvdr/javafx8sandbox/WebSocketsApplication.java
@@ -0,0 +1,34 @@
+package nl.mvdr.javafx8sandbox;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX application containing a web view. Displays a website in a JavaFX application.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class WebSocketsApplication extends Application {
+    /** {@inheritDoc} */
+    @Override
+    public void start(Stage primaryStage) {
+        WebView view = new WebView();
+        view.getEngine().load("http://www.websocket.org/echo.html");
+        
+        primaryStage.titleProperty().bind(view.getEngine().titleProperty());
+        primaryStage.setScene(new Scene(view));
+        primaryStage.show();
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args
+     *            commandline parameters; these are passed on to JavaFX
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
