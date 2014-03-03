64bef342dfdbcfe902433379ad3a449f307a7edb
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-03 10:59:43 +0100

    Javadoc improvements.

--- src/main/java/nl/mvdr/javafx8sandbox/WebWorkerApplication.java
+++ src/main/java/nl/mvdr/javafx8sandbox/WebWorkerApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 /**
- * JavaFX application containing a web view. Displays a website in a JavaFX application.
+ * JavaFX application containing a web view. Displays a website featuring HTML 5 Web Workers in a JavaFX application.
  * 
  * @author Martijn van de Rijdt
  */
