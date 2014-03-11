3b1020ce12d9d68276c6d480c91f9ab048b0be4a
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-03-11 18:04:15 +0100

    Using a label rather than System.out.

--- src/main/java/nl/mvdr/javafx8sandbox/LambdaApplication.java
+++ src/main/java/nl/mvdr/javafx8sandbox/LambdaApplication.java
@@ -3,6 +3,8 @@ package nl.mvdr.javafx8sandbox;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -14,21 +16,23 @@ public class LambdaApplication extends Application {
     /** {@inheritDoc} */
     @Override
     public void start(Stage primaryStage) {
+        Label label = new Label("Button not pressed.");
+        
         Button button = new Button("Press me!");
 
         // Without lambdas we would have to write something like:
 //        button.setOnAction(new EventHandler<ActionEvent>() {
 //            @Override
 //            public void handle(ActionEvent event) {
-//                System.out.println("Button pressed.");
+//                label.setText("Button pressed.");
 //            }
 //        });
         
         // With lambdas we can simply write:
-        button.setOnAction(event -> System.out.println("Button pressed."));
+        button.setOnAction(event -> label.setText("Button pressed."));
         
         primaryStage.setTitle("Lambda");
-        primaryStage.setScene(new Scene(button));
+        primaryStage.setScene(new Scene(new VBox(button, label)));
         primaryStage.show();
     }
 
